b4137b16ad2264871426788b89279ee58239f199
Arch Wu <archwu0817@gmail.com>
2020-02-05 16:55:21 -0800

    minor modifications to TestCheckout

--- assignment4/src/TestCheckout.java
+++ assignment4/src/TestCheckout.java
@@ -14,9 +14,10 @@ public class TestCheckout {
 
         checkout.clear();
 
-        checkout.enterItem(new Candy("Gummy Worms", 1.33, 89));
-        checkout.enterItem(new IceCream("Strawberry Ice Cream", 105));
+
+        checkout.enterItem(new IceCream("Strawberry Ice Cream", 145));
         checkout.enterItem(new Sundae("Vanilla Ice Cream", 105, "Caramel", 50));
+        checkout.enterItem(new Candy("Gummy Worms", 1.33, 89));
         checkout.enterItem(new Cookie("Chocolate Chip Cookies", 4, 399));
         checkout.enterItem(new Candy("Salt Water Taffy", 1.5, 209));
         checkout.enterItem(new Candy("Candy Corn", 3.0, 109));
