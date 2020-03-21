43868f53f1aeb07442d34a33d191bd960fea7d5e
Arch Wu <archwu0817@gmail.com>
2020-03-21 16:42:13 -0700

    ReverseHello done

--- assignment7/src/ReverseHello.java
+++ assignment7/src/ReverseHello.java
@@ -1,28 +1,56 @@
 public class ReverseHello {
   public static void main(String[] args) {
-    rip(50);
+    rip2(50);
   }
 
-  public static void rip(int threadNum) {
-    ThreadRipper[] threads = new ThreadRipper[threadNum];
-    Thread[] pool = new Thread[threadNum];
-    for (int i = 0; i < threadNum; i++) {
-      threads[i] = new ThreadRipper(i);
-      pool[i] = new Thread(threads[i]);
-      pool[i].start();
-      threads[i].sayHello();
-    }
+//  public static void rip(int threadNum) {
+//    ThreadRipper[] threads = new ThreadRipper[threadNum];
+//    Thread[] pool = new Thread[threadNum];
+//    for (int i = 0; i < threadNum; i++) {
+//      threads[i] = new ThreadRipper(i, 0);
+//      pool[i] = new Thread(threads[i]);
+//      pool[i].start();
+//      threads[i].start();
+//    }
+//  }
+
+  public static void rip2(int threadNum) {
+    ThreadRipper threadRipper = new ThreadRipper(50, 1);
+    Thread thread = new Thread(threadRipper);
+    thread.start();
+//    synchronized (thread) {
+//      try {
+//        thread.run();
+//        thread.join();
+//      } catch (InterruptedException e) {
+//        e.printStackTrace();
+//      }
+//    }
   }
 }
 
 class ThreadRipper extends Thread {
   private int numberOfThread;
+  private int curNum;
 
-  public ThreadRipper(int numberOfThread) {
+  public ThreadRipper(int numberOfThread, int curNum) {
     this.numberOfThread = numberOfThread;
+    this.curNum = curNum;
   }
 
-  public void sayHello() {
-    System.out.println("Hello from thread " + numberOfThread);
+  public void run() {
+    if (curNum > 50) {
+      return;
+    }
+    Thread thread = new Thread(new ThreadRipper(50, curNum + 1));
+    thread.start();
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Hello from thread " + curNum);
+
   }
 }
